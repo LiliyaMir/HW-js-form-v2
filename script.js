3d047b6d068904b9043c09c1de5a0df0f26a56d3
Lilia Mironova <lilyamironova67@gmail.com>
2022-04-22 12:57:13 +0300

    finish

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 let addButton = document.querySelector('.add_button');
-let toDo = document.querySelector('.to_do');
+let input = document.querySelector('.to_do'); 
 
 function createTodo() {
   const li = document.createElement("li");
@@ -16,21 +15,45 @@ function createTodo() {
   li.appendChild(span);
 }
 
+function stopDefAction(evt) {
+  evt.preventDefault();
+}
+addButton.addEventListener('click', stopDefAction, false);
 addButton.addEventListener("click", function() {
-  createTodo();
-  toDo.value = ''; 
+  if(input.validity.valid) {
+    createTodo();
+    input.value = ''; 
+  }
+  // createTodo();
+  // input.value = ''; 
 });
 
 ul.addEventListener('click', function (event) {
   let item = event.target.closest('li');
   if (item) {
-    item.classList.toggle('fulfilled')
+    item.classList.toggle('fulfilled');
   }
 })
-
 ul.addEventListener('click', function (event) {
   let item = event.target.closest('.close');
   if (item) {
     item.parentNode.remove();
   }
 })
+
+const form  = document.querySelector('.to_do_form');
+const inputError = document.querySelector('.to_do + span.error');
+
+input.addEventListener('input', function() {
+  if (input.validity.valid) {
+    inputError.textContent = ''; 
+    inputError.className = 'error'; 
+  } 
+  else {
+    showError();
+  }
+});
+function showError() {
+  inputError.className = 'error active';
+  inputError.textContent = 'invalid'; 
+}
